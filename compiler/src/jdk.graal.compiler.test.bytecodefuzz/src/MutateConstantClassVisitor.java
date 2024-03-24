4e551b516b04d2cbac67cd650d5d4a807707a551
Jan Papesch <janpapuch@seznam.cz>
2024-03-24 12:30:27 +0100

    plugged into Jazzer mutators

---
+++ compiler/src/jdk.graal.compiler.test.bytecodefuzz/src/MutateConstantClassVisitor.java
@@ -0,0 +1,74 @@
+package jdk.graal.compiler.test.bytecodefuzz;
+
+import java.util.Optional;
+import java.lang.reflect.AnnotatedType;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+
+import com.code_intelligence.jazzer.mutation.api.MutatorFactory;
+import com.code_intelligence.jazzer.mutation.api.PseudoRandom;
+import com.code_intelligence.jazzer.mutation.api.SerializingMutator;
+import com.code_intelligence.jazzer.mutation.mutator.lang.LangMutators;
+import com.code_intelligence.jazzer.mutation.engine.SeededPseudoRandom;
+import com.code_intelligence.jazzer.mutation.support.TypeSupport;
+
+import com.code_intelligence.jazzer.mutation.annotation.WithUtf8Length;
+import com.code_intelligence.jazzer.mutation.support.TypeHolder;
+
+public class MutateConstantClassVisitor extends ClassVisitor {
+
+    PseudoRandom prng;
+    int api;
+
+    public MutateConstantClassVisitor(int api, ClassVisitor classVisitor, int seed) {
+        super(api, classVisitor);
+        this.prng = new SeededPseudoRandom(seed);
+        this.api = api;
+        this.cv = classVisitor;
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+        return new MutateConstantMethodVisitor(api, this.cv.visitMethod(access, name, descriptor, signature, exceptions), prng);
+    }
+    
+    class MutateConstantMethodVisitor extends MethodVisitor {
+
+        PseudoRandom prng;
+        MutatorFactory mutatorFactory = LangMutators.newFactory();
+
+        public MutateConstantMethodVisitor(int api, MethodVisitor methodVisitor, PseudoRandom prng) {
+            super(api, methodVisitor);
+            this.mv = methodVisitor;
+            this.prng = prng;
+        }
+
+
+        <T> T mutateOrThrow(Object value, Class<T> clazz) throws Exception {
+            AnnotatedType annotatedType = TypeSupport.asAnnotatedType(clazz);
+
+            if (value instanceof String s) {
+                // Divided by 2 because utf16 representation of utf8 can be up to 2 times larger, and we need to keep the size of the classfile small
+                // TODO: perhaps allow the string to grow a little?
+                annotatedType = JazzerTypeSupport.WithUtf8Length(annotatedType, 0, s.length() / 2);
+            }
+
+            @SuppressWarnings("unchecked") // This method will throw before the cast would be unsuccesfull
+            SerializingMutator<T> mutator = (SerializingMutator<T>)mutatorFactory.createOrThrow(annotatedType);
+            return mutator.mutate(clazz.cast(value), prng);
+        }
+
+        @Override
+        public void visitLdcInsn(Object value){
+            try {
+                Object mutant = mutateOrThrow(value, value.getClass());
+                this.mv.visitLdcInsn(mutant);
+            }
+            catch (Exception e) {
+                this.mv.visitLdcInsn(value);
+            }
+        }
+
+    }
+}
--- compiler/src/jdk.graal.compiler.test.bytecodefuzz/src/Mutator.java
+++ compiler/src/jdk.graal.compiler.test.bytecodefuzz/src/Mutator.java
@@ -1,6 +1,8 @@
 package jdk.graal.compiler.test.bytecodefuzz;
 
 import java.lang.Exception;
+import java.io.StringWriter;
+import java.io.PrintWriter;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.tree.ClassNode;
@@ -8,8 +10,11 @@ import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.LdcInsnNode;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.util.Printer;
 import org.objectweb.asm.util.Textifier;
 import org.objectweb.asm.util.TraceMethodVisitor;
+import org.objectweb.asm.util.TraceClassVisitor;
+
 
 public final class Mutator {
 
@@ -19,22 +24,11 @@ public final class Mutator {
         ClassWriter writer = new ClassWriter(0);
         MutateConstantClassVisitor mutateConstants = new MutateConstantClassVisitor(Opcodes.ASM9, writer, seed);
 
-        byte[] result;
-        int maxIters = 10;
-        int iter = 0;
-
-        do {
-            reader.accept(mutateConstants, ClassReader.EXPAND_FRAMES);
-            result = writer.toByteArray();
-            iter++;
-            if (iter > maxIters) {
-                // TODO: think of what to do!
-                break;
-            }
-        } while (result.length > maxSize);
-        
+        reader.accept(mutateConstants, 0);
+        byte[] result = writer.toByteArray();
 
         if (result.length > maxSize) {
+            dumpBytecode(result);            
             // TODO: change into more appropriate exception type
             throw new Exception(String.format("Generated Classfile that is too long! Length %d > Max %d", result.length, maxSize));
         }
@@ -44,5 +38,13 @@ public final class Mutator {
 
     public void Init(int seed){
         return;
+    }
+
+    private void dumpBytecode(byte[] bytecode) {
+        ClassReader reader = new ClassReader(bytecode);
+        StringWriter sw = new StringWriter();
+        TraceClassVisitor visitor = new TraceClassVisitor(new PrintWriter(sw));
+        reader.accept(visitor, 0);
+        System.err.println(sw.toString());
     }
 }
