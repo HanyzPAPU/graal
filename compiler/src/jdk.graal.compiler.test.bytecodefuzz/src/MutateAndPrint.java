b86162f0bc034c773755dcb7487ca188153c4cf4
Jan Papesch <janpapuch@seznam.cz>
2024-06-16 13:21:06 +0200

    added duffs devide test case

--- compiler/src/jdk.graal.compiler.test.bytecodefuzz/src/MutateAndPrint.java
+++ compiler/src/jdk.graal.compiler.test.bytecodefuzz/src/MutateAndPrint.java
@@ -1,10 +1,13 @@
 package jdk.graal.compiler.test.bytecodefuzz;
 
 import java.lang.Exception;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.StringWriter;
 import java.io.PrintWriter;
+
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.tree.ClassNode;
@@ -29,6 +32,7 @@ public class MutateAndPrint {
         byte[] data = Files.readAllBytes(Path.of(".", "precorpus", filename));
 
         dumpBytecode(data);
+        run(data);
 
         ClassReader reader = new ClassReader(data);
         ClassWriter writer = new ClassWriter(0);
@@ -41,6 +45,7 @@ public class MutateAndPrint {
         assert(freeSpace.Amount() == maxSize - result.length);
 
         dumpBytecode(result);
+        run(result);
     }
 
     private static void dumpBytecode(byte[] bytecode) {
@@ -49,5 +54,43 @@ public class MutateAndPrint {
         TraceClassVisitor visitor = new TraceClassVisitor(new PrintWriter(sw));
         reader.accept(visitor, 0);
         System.err.println(sw.toString());
+    }
+
+    private static void run(byte[] bytecode) throws Exception {
+        
+        Object reciever;
+        ByteClassLoader loader = new ByteClassLoader();
+        Method method;
+
+        try {
+            Class<?> clazz = loader.LoadFromBytes(null, bytecode);
+
+            var methods = clazz.getMethods();
+            if (methods.length == 0) {
+                methods = clazz.getDeclaredMethods();
+                if (methods.length == 0) {
+                    throw new RuntimeException("Class does not contain any methods!");
+                }
+            }
+            
+            // For now select the first method
+            method = methods[0];
+            
+            reciever = (method.getModifiers() & Modifier.STATIC) != 0 ? null : clazz.getConstructor().newInstance();
+        }
+        // Don't crash on reflection/loading errors
+        catch (Exception ingored) {
+            return;
+        }
+        catch (java.lang.ClassFormatError ignored){
+            return;
+        }
+        catch (java.lang.NoClassDefFoundError ignored){
+            return;
+        }
+
+        Object res = method.invoke(reciever);
+
+        System.out.println(res);
     }
 }
